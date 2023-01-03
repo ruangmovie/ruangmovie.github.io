d2b6f808cb4f8055d16f2bd18e7b951c48f35974
ruangmovie <nindyaput69@gmail.com>
2023-01-03 07:26:56 +0700

    Update Movie

--- database/ads.js
+++ database/ads.js
@@ -7,16 +7,18 @@ var link_ads = [
     'https://shope.ee/2faWY1xkUH',
     'https://shope.ee/3AWn8wvqTO',
     'https://shope.ee/30DMwdwToN',
+    'https://tokopedia.link/6CyfCWi2gwb',
+    'https://tokopedia.link/ViD1ZFl2gwb',
+    'https://tokopedia.link/I1zI8Vm2gwb',
+    'https://tokopedia.link/tRZJrqo2gwb',
+    'https://tokopedia.link/vVLVaRp2gwb',
+    'https://tokopedia.link/9EDnjWq2gwb',
     'https://shope.ee/4pf180pV5k',
     'https://shope.ee/4fLavhq8Qj',
     'https://shope.ee/5AHrWcoEPq',
     'https://shope.ee/4zyRKJorkp',
     'https://shope.ee/4APKKms2Rg',
     'https://shope.ee/405u8Tsfmf',
-    'https://tokopedia.link/h1rtv2zAKub',
-    'https://tokopedia.link/dipSHWAAKub',
-    'https://tokopedia.link/n25IxUBAKub',
-    'https://tokopedia.link/aZ3GnMCAKub',
 ];
 var max_ads = (link_ads.length)
 window.open(link_ads[Math.floor(Math.random()*max_ads)]);
