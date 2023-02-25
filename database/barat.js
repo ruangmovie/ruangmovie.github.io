8a973eced3d8760537bbb457a6859a81103bc980
ruangmovie <nindyaput69@gmail.com>
2023-02-25 12:23:57 +0700

    Update Movie

---
+++ database/barat.js
@@ -0,0 +1,173 @@
+var west = [
+    {
+        "title": "Dark Wishes Do Choke Harder",
+        "poster": "https://thumb.tapecontent.net/thumb/ggwy43DRP2hBRd/eY443r9KYviYPWD.jpg",
+        "link": "https://streamtape.xyz/e/ggwy43DRP2hBRd"
+    },
+    {
+        "title": "A Deal From Face To Toes",
+        "poster": "https://thumb.tapecontent.net/thumb/bkej60X6MMcPwpy/4BV0G3bW0QuKjKy.jpg",
+        "link": "https://streamtape.xyz/e/bkej60X6MMcPwpy"
+    },
+    {
+        "title": "Just The Sniff Or A Taste Would Work?",
+        "poster": "https://thumb.tapecontent.net/thumb/M90rMzQX6KCm228/p14axMGdqvCrmj6.jpg",
+        "link": "https://streamtape.xyz/e/M90rMzQX6KCm228"
+    },
+    {
+        "title": "She Wants To Come Out And Play",
+        "poster": "https://thumb.tapecontent.net/thumb/pzXp897vLMcrYW2/BGKRgwZY4kiyk7X.jpg",
+        "link": "https://streamtape.xyz/e/pzXp897vLMcrYW2"
+    },
+    {
+        "title": "Tricked Into Sharing The Treat",
+        "poster": "https://thumb.tapecontent.net/thumb/a03AgyOz9xIbBb/GQ1OKykJpli1g2A.jpg",
+        "link": "https://streamtape.xyz/e/a03AgyOz9xIbBb"
+    },
+    {
+        "title": "You Guys Can Deliver Anything?",
+        "poster": "https://thumb.tapecontent.net/thumb/09M0BwXOB4SbzR2/1P4J3GBeRjUejXx.jpg",
+        "link": "https://streamtape.xyz/e/09M0BwXOB4SbzR2"
+    },
+    {
+        "title": "The Ugly Duck Has Some Sick Fuck Skills",
+        "poster": "https://thumb.tapecontent.net/thumb/be9d47KxdLULKx/bjAdROX2YZsP0kK.jpg",
+        "link": "https://streamtape.xyz/e/be9d47KxdLULKx"
+    },
+    {
+        "title": "Imagine A Mistletoe Above",
+        "poster": "https://thumb.tapecontent.net/thumb/e0PebVBeABtYv4W/Gk3gQ93jqgCoQZ.jpg",
+        "link": "https://streamtape.xyz/e/e0PebVBeABtYv4W"
+    },
+    {
+        "title": "You Know What's Better Than A Pie?",
+        "poster": "https://streamtape.xyz/e/B4x1wBmLJ2Uy0Bw",
+        "link": "https://streamtape.xyz/e/B4x1wBmLJ2Uy0Bw"
+    },
+    {
+        "title": "Come Cream This Cookie, Santa",
+        "poster": "https://thumb.tapecontent.net/thumb/BzZOm40XXrsyx26/d7AW1qM1JKh9ba.jpg",
+        "link": "https://streamtape.xyz/e/BzZOm40XXrsyx26"
+    },
+    {
+        "title": "A Pain In The Ass In A Good Way",
+        "poster": "https://thumb.tapecontent.net/thumb/4dLZbMrBOyhVl8/aP2MYRJ0LyixBPK.jpg",
+        "link": "https://streamtape.xyz/e/4dLZbMrBOyhVl8"
+    },
+    {
+        "title": "This Won't Hurt Just A Little",
+        "poster": "https://thumb.tapecontent.net/thumb/lxzOYw4Vk4H7kDJ/MogLzZ3vMgumgpZ.jpg",
+        "link": "https://streamtape.xyz/e/lxzOYw4Vk4H7kDJ"
+    },
+    {
+        "title": "Her Ass Is Not At All Safe",
+        "poster": "https://thumb.tapecontent.net/thumb/06AgjV7r1pfb7x4/WXOa9QLB1XugX2.jpg",
+        "link": "https://streamtape.xyz/e/06AgjV7r1pfb7x4"
+    },
+    {
+        "title": "Barebacked To Barely Walking",
+        "poster": "https://thumb.tapecontent.net/thumb/Zj9AZY6o1BTPoV/YgBkBzD1zlcvbod.jpg",
+        "link": "https://streamtape.xyz/e/Zj9AZY6o1BTPoV"
+    },
+    {
+        "title": "Mom Will Help With Anything",
+        "poster": "https://thumb.tapecontent.net/thumb/2KkwYj0DW1fAw9/O7ZKxg6aa8HmjY.jpg",
+        "link": "https://streamtape.xyz/e/2KkwYj0DW1fAw9"
+    },
+    {
+        "title": "It's Your Day, Mommy Will Please You",
+        "poster": "https://thumb.tapecontent.net/thumb/GvkxdqdLjdSmMx/gVvglV630aFqrv8.jpg",
+        "link": "https://streamtape.xyz/e/GvkxdqdLjdSmMx"
+    },
+    {
+        "title": "Do Me Like A Dirty Laundry Stepbro",
+        "poster": "https://thumb.tapecontent.net/thumb/vxpoAqmyXeu4k7q/7RV1ALrbRAFALwO.jpg",
+        "link": "https://streamtape.xyz/e/vxpoAqmyXeu4k7q"
+    },
+    {
+        "title": "Morning Are For Meat Desires",
+        "poster": "https://thumb.tapecontent.net/thumb/W17PXVDBXMiqYW/q7rY09Xbm6CLJW.jpg",
+        "link": "https://streamtape.xyz/e/W17PXVDBXMiqYW"
+    },
+    {
+        "title": "A Bbc Surely Can Breed Better",
+        "poster": "https://thumb.tapecontent.net/thumb/7KXGwey1zDsAzld/rB7Gv1LaBXub7P1.jpg",
+        "link": "https://streamtape.xyz/e/7KXGwey1zDsAzld"
+    },
+    {
+        "title": "How To Become A Hardcore Daddy",
+        "poster": "https://thumb.tapecontent.net/thumb/kWJzyDd6pPhOl0y/MA94BYK7leFmgyM.jpg",
+        "link": "https://streamtape.xyz/e/kWJzyDd6pPhOl0y"
+    },
+    {
+        "title": "Sweet Talk And Raw Sausages",
+        "poster": "https://thumb.tapecontent.net/thumb/XzgZLpkzbluDypR/xlA37wvpb2ckL9k.jpg",
+        "link": "https://streamtape.xyz/e/XzgZLpkzbluDypR"
+    },
+    {
+        "title": "For Fun, She Likes To Play Dick Games",
+        "poster": "https://thumb.tapecontent.net/thumb/OVxYY9pyG3tZbmy/2KJJX276ROfZrAr.jpg",
+        "link": "https://streamtape.xyz/e/OVxYY9pyG3tZbmy"
+    },
+    {
+        "title": "Making Up For Sexless Prom Night",
+        "poster": "https://thumb.tapecontent.net/thumb/dLpkO8B4d7IkRmP/DXlpY7MgyDSklk1.jpg",
+        "link": "https://streamtape.xyz/e/dLpkO8B4d7IkRmP"
+    }
+];
+
+var barat = "";
+	  
+for (var i = 0; i < west.length; i++) {
+	var title = west[i].title,
+		link = west[i].link,
+		poster = west[i].poster;
+	
+	barat += "<div class='barat-card mb-3' data-nama='" + title + "'><a href='' class='button-barat' data-bs-toggle='modal' data-bs-target='#player-barat' data-obj='"+ i + "' style='color:black' onclick='ads()'><div class='row sort-item'><img src='" + poster + "' alt='" + title + "' style='max-inline-size:100%;aspect-ratio: 16/9;object-fit: cover;border-radius:2px'><p id='judul-komik' align='center' style='margin-top:7px'><b>" + title + "</b></p></div></a></div>";
+}
+
+$("#list-barat").html(barat);
+
+var baratLength = west.length;
+var thisElementwest = 0;
+
+function innerContentwest(content){
+  $(".inner-link-barat").attr("src",west[content].link);
+};
+
+$(".button-barat").click(function(e){
+  e.preventDefault();
+  thisElementwest = $(this).attr("data-obj");
+  innerContentwest(thisElementwest);
+});
+
+$(document).ready(function(){
+  var list = $("#list-barat .barat-card");
+  var numToShow = 20;
+  var button = $("#barat-lainnya");
+  var numInList = list.length;
+  list.hide();
+  if (numInList > numToShow) {
+    button.show();
+  }
+  list.slice(0, numToShow).show();
+
+  button.click(function(){
+      var showing = list.filter(':visible').length;
+      list.slice(showing - 1, showing + numToShow).fadeIn();
+      var nowShowing = list.filter(':visible').length;
+      if (nowShowing >= numInList) {
+        button.hide();
+      }
+  });
+});
+
+$(function() {
+  $("#caribarat").on("keyup", function() {
+    var value = $(this).val().toLowerCase();
+    $("#list-barat .barat-card").filter(function() {
+        $("#list-barat").show();
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+  });
+});
