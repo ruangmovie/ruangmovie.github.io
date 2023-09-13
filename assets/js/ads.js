27b94824255ee3f95cf0965784a50e11e923fae2
ruangmovie <nindyaput69@gmail.com>
2023-09-13 16:18:06 +0700

    Update Projects

---
+++ assets/js/ads.js
@@ -0,0 +1,32 @@
+var ads = function(){
+var link_ads = [
+    'https://maspannn.github.io',
+    'https://shope.ee/3pmTwAtJ7S',
+    'https://shope.ee/3fT3jrtwSR',
+    'https://shope.ee/2ptwkKx79I',
+    'https://shope.ee/2faWY1xkUH',
+    'https://shope.ee/3AWn8wvqTO',
+    'https://shope.ee/30DMwdwToN',
+    'https://shope.ee/4pf180pV5k',
+    'https://shope.ee/4fLavhq8Qj',
+    'https://shope.ee/5AHrWcoEPq',
+    'https://shope.ee/4zyRKJorkp',
+    'https://shope.ee/4APKKms2Rg',
+    'https://shope.ee/405u8Tsfmf',
+    'https://tokopedia.link/XCihkticTub',
+    'https://tokopedia.link/nrSsahxAKub',
+    'https://tokopedia.link/h1rtv2zAKub',
+    'https://tokopedia.link/aZ3GnMCAKub',
+    'https://tokopedia.link/rKwX5uDAKub',
+    'https://tokopedia.link/XL4m7rEAKub',
+    'https://tokopedia.link/VliU3gFAKub',
+    'https://tokopedia.link/gXjQA9FAKub',
+    'https://tokopedia.link/x5tmrTGAKub',
+    'https://tokopedia.link/dV6bnFHAKub',
+    'https://tokopedia.link/LgieBnIAKub',
+    'https://tokopedia.link/hw58GUGpdub',
+    'https://tokopedia.link/4Gth3wKpdub',
+];
+var max_ads = (link_ads.length)
+window.open(link_ads[Math.floor(Math.random()*max_ads)]);
+};
