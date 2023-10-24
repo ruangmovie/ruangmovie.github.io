c804aa24b4c17332d766c9f4cff6c27848ef733b
ruangmovie <nindyaput69@gmail.com>
2023-10-25 02:17:21 +0700

    Update Asupan

---
+++ database/shopee.js
@@ -0,0 +1,123 @@
+var data = [
+    {
+        "nama": "PAKET 4PCS 100RB Kaos Branded Distro Pria-Wanita Size M-L-XL-XXL PE [ATWOZI01]",
+        "harga": 94905,
+        "photo": "https://down-aka-id.img.susercontent.com/id-11134207-7r98x-ll3ihjtjz6ep8e.webp",
+        "link": "https://shope.ee/9zWZahb6wK"
+    },
+    {
+        "nama": "Hoodie Sweater Pria Wanita Distro Warna Abu Paralon Hodi Murah Jaket Tebal Authentic Terbaru 2023 Keren Hodie Unisex Switer Hode Denim Sablon List Dont Care Cowok Terpopuler Aesthetic Original Hudi Jumbo Rapture Clothing Hudie Premium Lokal Size M L XLXXl",
+        "harga": 65800,
+        "photo": "https://down-id.img.susercontent.com/file/sg-11134201-23010-mjtphyx0rvlvb5",
+        "link": "https://shope.ee/A9pzn0aTbN"
+    },
+    {
+        "nama": "Celana Pendek Pria Broadshort Premium Harian Santai",
+        "harga": 19670,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qul2-lj2u37s9ikjf29",
+        "link": "https://shope.ee/AK9PzJZqGQ"
+    },
+    {
+        "nama": "BONUS DADAKAN Celana Joger pria wanita panjang training trening trining trening Jogger Gym olahraga jumbo style korea SIZE 27-38",
+        "harga": 90000,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qula-lj6rjtk6go0meb",
+        "link": "https://shope.ee/AUSqBcZCvT"
+    },
+    {
+        "nama": "Borongdong - Kaos Kaki Pendek Atas Mata Kaki Pria Wanita/Kaos Kaki Olahraga/Kaos Kaki Sport (New Sport 3/4)",
+        "harga": 2900,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qukw-lgisfkx2hpse9a",
+        "link": "https://shope.ee/9KGsnTdeIG"
+    },
+    {
+        "nama": "TERBARU... 6 PCS CELANA DALAM PRIA TERMURAH HIGH QUALITY/CD PRIA SEGITIGA/CD MURAH/BAHAN COTTON/UKURAN M L XL XXL",
+        "harga": 33250,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qul9-ljjy3avemfh4db",
+        "link": "https://shope.ee/9UaIzmd0xJ"
+    },
+    {
+        "nama": "Baju Atasan Kaos T-Shirt Pria Distro THE INDUATION Lengan Pendek Terbaru Fox Distro Bahan Katun Combed",
+        "harga": 44729,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qul1-lkfgrq3qpsjac8",
+        "link": "https://shope.ee/9etjC5cNcM"
+    },
+    {
+        "nama": "Kaos Kaki Angkle Tebal Sport Pria Wanita",
+        "harga": 4510,
+        "photo": "https://down-id.img.susercontent.com/file/c24bdf1fac769916b6c4024b970fb516",
+        "link": "https://shope.ee/9pD9OObkHP"
+    },
+    {
+        "nama": "Onixe Inside Jaket Rugby Pria & Wanita (UNISEX) Kombinasi Original High Quality Distro(Bisa COD)",
+        "harga": 79500,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qul7-lhh730xtyxu6ec",
+        "link": "https://shope.ee/8f1C0FgBeC"
+    },
+    {
+        "nama": "Terwuwu kemeja panjang pria cowok gatlemen cowok biru navy dongker polos formal kantor",
+        "harga": 25650,
+        "photo": "https://down-id.img.susercontent.com/file/4f88c6bedbbd8f41769749686cbf84c1",
+        "link": "https://shope.ee/8pKcCYfYJF"
+    },
+    {
+        "nama": "MISS'O BX355 Celana Dalam Boxer Pria Cowok Lelaki Motif Polos",
+        "harga": 5127,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7r98s-lmu5bk4twlgr7b",
+        "link": "https://shope.ee/8ze2OreuyI"
+    },
+    {
+        "nama": "Clote Kaos Salur Pocket Cotton Combed 24s",
+        "harga": 54666,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qula-lkfhu4iw0uk604",
+        "link": "https://shope.ee/99xSbAeHdL"
+    },
+    {
+        "nama": "Woopie Kaos Salur Pocket Biggest Cotton Combed 24s",
+        "harga": 57900,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7quky-lji2i5yglke7c3",
+        "link": "https://shope.ee/7zlVD1ij08"
+    },
+    {
+        "nama": "Jaket hoodie cowok bahan tebal Hoodie pria distro aesthetic warna mint Sweater hodie distro aesthetic sablon freaks Sweater cowok dewasa terbaru 2023 Jaket pria branded Jaket pria hoodie distro tebal murah Ogik Hoodie wanita korean style lucu",
+        "harga": 94500,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134207-7qul2-lk87smik4y7e3b",
+        "link": "https://shope.ee/8A4vPKi5fB"
+    },
+    {
+        "nama": "[TERLARIS] CELANA CARGO PANJANG PRIA UKURAN 27-38 PREMIUM QUALITY SLIMFIT COWO SKINNY POLOS COWOK",
+        "harga": 51299,
+        "photo": "https://down-id.img.susercontent.com/file/id-11134601-7r98o-lkt52m9fkz5u86",
+        "link": "https://shope.ee/8KOLbdhSKE"
+    },
+    {
+        "nama": "KAOS DISTRO PRIA / KAOS DISTRO COWOK / KAOS DISTRO BANDUNG / NATIVE8 - THE FOREST",
+        "harga": 47500,
+        "photo": "https://down-id.img.susercontent.com/file/sg-11134201-23010-43gseey9afmvd5",
+        "link": "https://shope.ee/8UhlnwgozH"
+    },
+    {
+        "nama": "BAPIN Celana Kantor Celana Chino",
+        "harga": 141900,
+        "photo": "https://down-id.img.susercontent.com/file/6168337271ecd7d8df6882f89af674eb",
+        "link": "https://shope.ee/7KVoPnlGM4"
+    },
+    {
+        "nama": "Paket 7 Bonus 1 pcs CD Pria Celana Dalam Laki-Laki Bahan katun terbaru",
+        "harga": 33600,
+        "photo": "https://down-id.img.susercontent.com/file/sg-11134201-23010-27ae407gscmva2",
+        "link": "https://shope.ee/7UpEc6kd17"
+    },
+    {
+        "nama": "VCRL - Celana Jogger Pria Trendy Korean Style Ukuran Jumbo",
+        "harga": 54000,
+        "photo": "https://down-id.img.susercontent.com/file/sg-11134201-23010-27ae407gscmva2",
+        "link": "https://shope.ee/7f8eoPjzgA"
+    }
+];
+
+function randomads() {
+  let random = data[Math.floor(Math.random() * data.length)];
+  iklan.innerHTML = `<div class="container"><div class="row"><div class="col-2"><img src="${random.photo}" onerror="this.onerror=null;this.src='assets/img/notfound.jpeg';" class="rounded float-start" alt="Rekomendasi Produk Shopee Murah" width="73px" height="73px" style="margin-left:-7px"></div><div class="col-10" style="padding-left:20px"><p class="judul-iklan" style="font-size:11pt;margin-top:-4px">${random.nama}</p><div class="row"><div class="col" style="margin-top:-12px"><p class="rupiah">Rp</p><p class="d-inline-block" style="color:#EE4D2D;font-size:16pt">${random.harga}</p></div><div class="col"><a href="${random.link}" class="position-absolute bottom-0 end-0"><button type="button" class="btn" style="margin-bottom:8px;margin-right:8px;font-size:9pt;background-color:#EE4D2D;color:white">Beli Sekarang</button></a></div></div></div></div></div>`;
+  ad.innerHTML = random.ad;
+}
+randomads();
