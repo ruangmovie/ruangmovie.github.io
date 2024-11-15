76bb41082c2f6cb30beac2659ce5771563cdd81a
ruangmovie <nindyaput69@gmail.com>
2024-11-15 10:35:40 +0700

    Update Movie

--- database/clips.js
+++ database/clips.js
@@ -2,7 +2,4 @@ var clips = [
     {
         "link": "https://pt.cdwmtt.com/live-stream/fk/?c=object_container&site=wl3&cobrandId=241040&psid=oklivetv&pstool=319_1&psprogram=cbrnd&campaign_id=94891&vp%5BshowChat%5D=false&vp%5BchatAutoHide%5D=false&vp%5BshowCallToAction%5D=false&vp%5BshowPerformerName%5D=false&vp%5BshowPerformerStatus%5D=false&subAffId=%7BSUBAFFID%7D&categoryName=girl&embedTool=1&origin=syndication.exdynsrv.com"
     },
-    {
-        "link": "https://syndication.exdynsrv.com/ads-iframe-display.php?idzone=3401685&type=300x250&p=https%3A//oklivetv.com/erox-hd-live/&dt=1706457652830&sub=&tags=&cookieconsent=true&screen_resolution=424x942&el=%22"
-    },
 ];
